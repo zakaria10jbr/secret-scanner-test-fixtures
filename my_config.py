88e0cd89399db0c263c913aaa3904c52c70115cc
Your Name <your@email.com>
2026-08-19 14:46:15 +0100

    add new fake token to test baseline new-finding detection

--- my_config.py
+++ my_config.py
@@ -6,8 +6,12 @@ api_key = "PUT_YOUR_KEY_HERE_1234567890"
 
 username = "admin_user"
 
-
+github_token = "ghp_fakeTokenForTesting1234567890ab"
 slack_token = "xoxb-fake-slack-token-1234567890"
 github_token = "ghp_aB3xK9mQ2vL8pN4zR7wJ6tY1cF5dH0b"
 stripe_key = "sk_live_51H8xJ2eZvKYlo2C4nQm9pR3wS7tU1v"
 db_url = "postgres://admin:P4ssw0rd123@dbserver01:5432/production"
+
+
+new_api_token = "ghp_zZ9xK2mQ8vL4pN6wJ7tY3cF1dH5b0aB"
+
